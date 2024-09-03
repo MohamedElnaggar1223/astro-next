a12699574ec104f7a93fd19855f4fefd112a1179
MohamedElnaggar1223 <maelnaggar1223@gmail.com>
2024-09-03 23:38:42 +0300

    First

---
+++ src/nexts/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: '/docs',
+    // output: 'export',
+    // distDir: '../../dist/next',
+  };
+  
+export default nextConfig;
